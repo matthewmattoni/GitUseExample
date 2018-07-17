34768f67361ffb1efae9fc6c7424d371b2ced1d1
mpmattoni <matt.mattoni@magwareapps.com>
2018-07-17 15:51:17 -0600

    Added new map feature.

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,14 @@
+////
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Matthew Mattoni on 7/17/18.
+//  Copyright © 2018 MagWareApps, LLC. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary {
+    
+    
+}
